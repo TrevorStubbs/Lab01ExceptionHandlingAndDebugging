4484acc955f7ffc26936ca5265cf9e0b9fb6c69c
Trevor Stubbs <stubbste@gmail.com>
2020-07-06 16:45:26 -0700

    Comments completed

--- Lab01ExceptionHandlingAndDebugging/Program.cs
+++ Lab01ExceptionHandlingAndDebugging/Program.cs
@@ -4,18 +4,22 @@ namespace Lab01ExceptionHandlingAndDebugging
 {
     class Program
     {
+        // Insertion into the whole application
         static void Main(string[] args)
         {
             try
             {
+                // Run the Start Sequence
                 StartSequence();
             }
             catch (Exception e)
             {
+                // Catch and display the error
                 Console.WriteLine($"Exception caught: {e.Message}");
             }
             finally
             {
+                // Tell the user that the app is completed
                 Console.WriteLine("Application Complete.");
             }
         }
@@ -25,20 +29,28 @@ namespace Lab01ExceptionHandlingAndDebugging
 
             try
             {
+                // Intro Messages
                 Console.WriteLine("Welcome to my game! Let's do some math!");
-
                 Console.WriteLine("Enter a number greater than zero.");
+
+                // Get a number from the user to define the size of the array
                 int numberFromUser = Convert.ToInt32(Console.ReadLine());
 
+                // initialize an array based off the user's input
                 int[] initalArray = new int[numberFromUser];
 
+                // Run Populate method to fill the array
                 Populate(initalArray);
+
+                // run the app's methods and capture each output into variables
                 int outputFromGetSum = GetSum(initalArray);
                 int outputFromGetProduct = GetProduct(initalArray, outputFromGetSum);
                 decimal outputFromGetQuotient = GetQuotient(outputFromGetProduct);
 
+                // Print the size of the array
                 Console.WriteLine($"Your array is size: {numberFromUser}");
 
+                // Loop through each item and print them to the console
                 Console.Write("The numbers in the array are ");
                 for (int i = 0; i < initalArray.Length; i++)
                 {
@@ -49,6 +61,8 @@ namespace Lab01ExceptionHandlingAndDebugging
                 }
 
                 Console.WriteLine();
+
+                // Print the outcomes of the methods
                 Console.WriteLine($"The sum of the array is {outputFromGetSum}");
                 Console.WriteLine($"{outputFromGetSum} * {outputFromGetProduct/outputFromGetSum} = {outputFromGetProduct}");
                 Console.WriteLine($"{outputFromGetProduct} / {outputFromGetProduct / outputFromGetQuotient} = {outputFromGetQuotient}");              
@@ -56,18 +70,22 @@ namespace Lab01ExceptionHandlingAndDebugging
             }
             catch (FormatException)
             {
+                // Tell the user that they didn't enter a number
                 Console.WriteLine($"You did not enter a number");
             }
             catch (OverflowException)
             {
+                // Tell the user that the number was not a zero.
                 Console.WriteLine($"Your number was not greater than zero");
             }
             catch (Exception)
             {
+                // Whatever exception enters here let the main deal with it.
                 throw;
             }
         }
 
+        // This method populates the array with user defined values
         static int[] Populate(int[] inputArray)
         {
             for (int i = 0; i < inputArray.Length; i++)
@@ -78,6 +96,7 @@ namespace Lab01ExceptionHandlingAndDebugging
             return inputArray;
         }
 
+        // This method will sum all the values in the array together and output it into a single int
         static int GetSum(int[] inputArray)
         {
             int sum = 0;
@@ -85,12 +104,14 @@ namespace Lab01ExceptionHandlingAndDebugging
             foreach (int number in inputArray)
                 sum += number;
 
+            // if the sum is less than zero send a new exception through the chain.
             if (sum < 20)
                 throw (new Exception($"Value of {sum} is too low."));
 
             return sum;
         }
 
+        // Have the user select a random index from the array and have it be multiplied by the sum of the previous method.
         static int GetProduct(int[] inputArray, int inputSum)
         {
             int product;
@@ -110,6 +131,7 @@ namespace Lab01ExceptionHandlingAndDebugging
             return product;
         }
 
+        // Divide a user inputed number by the product produced by the previous method.
         static decimal GetQuotient(int inputNumber)
         {
             decimal convertInputToDec = Convert.ToDecimal(inputNumber);
