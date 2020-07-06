f4fd20565f489752a31b3b72601c4f84c30f4b24
Trevor Stubbs <stubbste@gmail.com>
2020-07-06 14:44:25 -0700

    start sequence outline created

--- Lab01ExceptionHandlingAndDebugging/Program.cs
+++ Lab01ExceptionHandlingAndDebugging/Program.cs
@@ -9,11 +9,11 @@ namespace Lab01ExceptionHandlingAndDebugging
             Console.WriteLine("Hello World!");
             try
             {
-                //StartSequence();
+                StartSequence();
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"Exception caught: {e.Message}");
             }
             finally
             {
@@ -22,16 +22,48 @@ namespace Lab01ExceptionHandlingAndDebugging
         }
 
         //StartSequence
-            //  static void (no params)
+        //  static void (no params)
+        static void StartSequence()
+        {
+            // User to Enter a number greater than zero
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+            Console.WriteLine("Enter a number greater than zero.");
+            int numberFromUser = Convert.ToInt32(Console.ReadLine());
+
+            int[] initalArray = new int[numberFromUser];
+            try
+            {
+                // Populate();
+                // int[] sums = GetSum(int[])
+                // int product = GetProduct(int[], sums)
+                // int quotient = getQuotient(product)
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"{numberFromUser} is not a number");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"{numberFromUser} is not greater than zero");
+            }
+            catch (Exception) // TODO find specific error
+            {
+                throw;
+            }
+        }
+
         //Populate
-            // static int[] (int [])
+        // static int[] (int [])
+
         //GetSum
-            // static int (int[])
+        // static int (int[])
+
         //GetProduct
-            // static int (int[])
+        // static int (int[])
+
         //Get Quotient
-            // static decimal (int [from GetProduct)
-            
+        // static decimal (int [from GetProduct)
 
     }
 }
