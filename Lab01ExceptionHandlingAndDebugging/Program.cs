fe62ed02f7685da9c205274ce7244c474e8f76fa
Trevor Stubbs <stubbste@gmail.com>
2020-07-06 14:12:34 -0700

    setup main and outline the rest of the app

--- Lab01ExceptionHandlingAndDebugging/Program.cs
+++ Lab01ExceptionHandlingAndDebugging/Program.cs
@@ -7,6 +7,31 @@ namespace Lab01ExceptionHandlingAndDebugging
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            try
+            {
+                //StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Application Complete.");
+            }
         }
+
+        //StartSequence
+            //  static void (no params)
+        //Populate
+            // static int[] (int [])
+        //GetSum
+            // static int (int[])
+        //GetProduct
+            // static int (int[])
+        //Get Quotient
+            // static decimal (int [from GetProduct)
+            
+
     }
 }
